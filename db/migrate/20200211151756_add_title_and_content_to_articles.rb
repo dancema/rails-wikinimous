c058fe9656ce9621c065cb7c150aabf3e069d3e8
DanCema <dan.cemachovic@edhec.com>
2020-02-11 17:00:23 +0100

    done

---
+++ db/migrate/20200211151756_add_title_and_content_to_articles.rb
@@ -0,0 +1,6 @@
+class AddTitleAndContentToArticles < ActiveRecord::Migration[5.2]
+  def change
+    add_column :articles, :title, :string
+    add_column :articles, :content, :text
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
+10.times do
+  Article.create(title: Faker::TvShows::BreakingBad.episode, content: Faker::TvShows::BreakingBad.character)
+end
+
+puts 'Completed'
