d8ac2bd18f8e56d037d514a6c27e7b438473bd60
DanCema <dan.cemachovic@edhec.com>
2020-02-11 17:31:54 +0100

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,17 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    raise
+    @article = Article.new
+  end
+
+  def create
+  end
 end
